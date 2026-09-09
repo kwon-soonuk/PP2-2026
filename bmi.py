afc04fe86a0f38c5a12bc4e8122d4e358b5900c1
kwon-soonuk <powersidsid@office.seowon.ac.kr>
2026-09-09 14:57:55 +0900

    BMI 계산 함수 및 테스트 추가

---
+++ bmi.py
@@ -0,0 +1,17 @@
+# bmi.py in week2
+# bmi 계산 함수
+# Body Mass Index (BmI) 계산 함수
+#
+
+def get_bmi(weight_kg:float, height_cm:float) -> float:
+    bmi = weight_kg / (height_cm/100) ** 2
+    return bmi
+
+def test_get_bmi():
+    height_cm = 177
+    weight_kg = 57
+    b = get_bmi(weight_kg, height_cm)
+    print(f"키({height_cm}) 몸무게({weight_kg}) BMI는 {b}입니다")
+
+if __name__ == __main__:
+    test_get_bmi()
